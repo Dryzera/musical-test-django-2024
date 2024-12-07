8dcf65100f5da0cc04f23d45a3713cd5750d98ee
Dryzera <endryusdaniel6321@gmail.com>
2024-12-07 17:28:22 -0300

    added urls names

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import index
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', view=index, name='main'),
+    # perfil urls
+    path('perfil/<int:pk>/resumegames/', view=index, name='resumegames'),
+    path('perfil/<int:pk>/', view=index, name='perfil'),
+
+    # game urls
+    path('game/', view=index, name='game'),
+    path('game/resume/', view=index, name='game_resume'),
+    path('games/', view=index, name='games'),
+
+    # generic urls
+    path('search/<slug:slug>/', view=index, name='search'),
+    path('', view=index, name='home'),
 ]
